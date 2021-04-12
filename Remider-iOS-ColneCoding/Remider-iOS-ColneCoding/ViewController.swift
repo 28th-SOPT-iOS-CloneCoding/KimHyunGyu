c83addf5106d0b2fe8267256e754a543d56c8beb
hyungyu Kim <ypsophila97@icloud.com>
2021-04-12 23:03:35 +0900

    Upload Remider-iOS-CloneCoding

---
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remider-iOS-ColneCoding
+//
+//  Created by kimhyungyu on 2021/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
