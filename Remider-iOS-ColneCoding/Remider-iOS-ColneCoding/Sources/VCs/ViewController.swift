56525c0bc5bfdc8c772d5c369b0d87949c24b4d6
hyungyu Kim <ypsophila97@icloud.com>
2021-04-13 23:52:18 +0900

    Commit

--- Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/ViewController.swift
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/ViewController.swift
@@ -18,7 +18,28 @@ class ViewController: UIViewController {
         searchController.searchBar.placeholder = "검색"
         navigationItem.searchController = searchController
         navigationItem.title = ""
-
+//
+//        //UIToolbar initialize
+//        
+//        let toolbar = UIToolbar(frame: .init(x: 0, y: 0, width: 100, height: 100))
+//        view.addSubview(toolbar)
+//        
+//        toolbar.translatesAutoresizingMaskIntoConstraints = false
+//        toolbar.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 0).isActive = true
+//        toolbar.bottomAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.bottomAnchor, multiplier: 0).isActive = true
+//        toolbar.trailingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.trailingAnchor, multiplier: 0).isActive = true
+//
+//        var items: [UIBarButtonItem] = []
+//
+//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+//        let toolbarItem1 = UIBarButtonItem(title: "새로운 미리 알림", style: .plain, target: self, action: nil)
+//        let toolbarItem2 = UIBarButtonItem(title: "목록 추가", style: .plain, target: self, action: nil)
+//
+//        items.append(toolbarItem1)
+//        items.append(flexibleSpace)
+//        items.append(toolbarItem2)
+//
+//        toolbar.setItems(items, animated: true)
     }
 
 
