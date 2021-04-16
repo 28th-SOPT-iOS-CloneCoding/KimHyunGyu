93b315945724cdd007f4d99847a4226252e08be4
hyungyu Kim <ypsophila97@icloud.com>
2021-04-16 18:24:12 +0900

    Add layout

---
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/AddListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddListViewController.swift
+//  Remider-iOS-ColneCoding
+//
+//  Created by kimhyungyu on 2021/04/16.
+//
+
+import Foundation
+import UIKit
+
+class AddListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func touchBackBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/ScheduleListVC.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleListVC.swift
+//  Remider-iOS-ColneCoding
+//
+//  Created by kimhyungyu on 2021/04/16.
+//
+
+import UIKit
+
+class ScheduleListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
