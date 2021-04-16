bae7e7f4977269937cb4fe90c63c977b37bd5f65
hyungyu Kim <ypsophila97@icloud.com>
2021-04-17 03:26:50 +0900

    [#6] 피드백 반영
    - toolbar 안에 barButtonItem 이 아닌 button 을 넣어서 이미지와 타이틀 설정
    - button 으로 이미지 만들어서 넣어줌.
    - separatorinset 속성으로 구분선 위치 변경

--- Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/Cells/CustomCell.swift
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/Cells/CustomCell.swift
@@ -13,11 +13,17 @@ class CustomCell: UITableViewCell {
     
     //MARK: - @IBOutlet Properties
     
+    @IBOutlet weak var listBulletBtn: UIButton!
     @IBOutlet weak var countLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        listBulletBtn.backgroundColor = .systemBlue
+        listBulletBtn.layer.cornerRadius = listBulletBtn.frame.height/2
+        listBulletBtn.setPreferredSymbolConfiguration(.init(pointSize: 12, weight: .bold, scale: .large), forImageIn: .normal)
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/AddListViewController.swift
+++ Remider-iOS-ColneCoding/Remider-iOS-ColneCoding/Sources/VCs/AddListViewController.swift
@@ -9,11 +9,25 @@ import Foundation
 import UIKit
 
 class AddListViewController: UIViewController {
+    @IBOutlet weak var listBulletBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setListBulletBtn()
     }
-    @IBAction func touchBackBtn(_ sender: Any) {
+    
+    //MARK: - @IBAction Properties
+    
+    @IBAction func touchCancelBtn(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: - Methods
+    
+    private func setListBulletBtn() {
+        listBulletBtn.backgroundColor = .systemBlue
+        listBulletBtn.layer.cornerRadius = listBulletBtn.frame.height/2
+        listBulletBtn.setPreferredSymbolConfiguration(.init(pointSize: 30, weight: .bold, scale: .large), forImageIn: .normal)
     }
 }
