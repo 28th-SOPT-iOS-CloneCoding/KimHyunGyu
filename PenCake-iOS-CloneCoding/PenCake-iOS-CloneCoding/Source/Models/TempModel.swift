d50dbb80f5e06889e6c43a1845b0ca5d0e591b82
hyungyu Kim <ypsophila97@icloud.com>
2021-05-27 02:24:49 +0900

    [#29] PenCake 프로젝트 설정
    ### Description
    - 폴더링
    
    ### Related Issue
    close #29

---
+++ PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/Models/TempModel.swift
@@ -0,0 +1,8 @@
+//
+//  TempModel.swift
+//  PenCake-iOS-CloneCoding
+//
+//  Created by kimhyungyu on 2021/05/27.
+//
+
+import Foundation
---
+++ PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/VCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PenCake-iOS-CloneCoding
+//
+//  Created by kimhyungyu on 2021/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
