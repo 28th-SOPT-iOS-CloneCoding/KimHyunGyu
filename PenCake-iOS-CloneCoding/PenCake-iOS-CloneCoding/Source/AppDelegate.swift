bf04bdc8f8c4538769a38e7da10522e0664c69b9
hyungyu Kim <ypsophila97@icloud.com>
2021-05-28 17:46:47 +0900

    [#31] Core Data 설정
    ### Description
    - Core Data Model 설정
    - Core Data Stack 설정
    
    ### Related Issue
    close #31

--- PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/AppDelegate.swift
+++ PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/AppDelegate.swift
@@ -6,16 +6,50 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
+            }
+        }
+        return container
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+//        if let rootVC = window?.rootViewController as? ViewController {
+//            rootVC.container = persistentContainer
+//        }
+        
         return true
     }
+    
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                
+            }
+            
+        }
+        
+    }
+
 
     // MARK: UISceneSession Lifecycle
 
--- PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/VCs/ViewController.swift
+++ PenCake-iOS-CloneCoding/PenCake-iOS-CloneCoding/Source/VCs/ViewController.swift
@@ -6,14 +6,76 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+//    var container: NSPersistentContainer!
+    
+    
+    
+    struct Story {
+        var title: String
+        var detail: String
+        var date: String
+    }
+    
+    // MARK: - @IBOutlet Properteis
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        guard container != nil else {
+//            fatalError("This view needs a persistent container.")
+//        }
+        
+        // The persistent container is available.
+//        setCoreData()
+        fetchStory()
     }
-
-
 }
 
+extension ViewController {
+    private func setCoreData() {
+        
+        let storyOne = Story(title: "일기2", detail: "디테일2", date: "20210528")
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "StoryModel", in: context)
+        
+        if let entity = entity {
+            let story = NSManagedObject(entity: entity, insertInto: context)
+            story.setValue(storyOne.title, forKey: "title")
+            story.setValue(storyOne.detail, forKey: "detail")
+            story.setValue(storyOne.date, forKey: "date")
+            
+            do {
+                try context.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func fetchStory() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        do {
+            let story = try context.fetch(StoryModel.fetchRequest()) as! [StoryModel]
+            story.forEach {
+                print($0.title)
+                print($0.detail)
+                print($0.date)
+            }
+        } catch {
+            print(error.localizedDescription)
+            
+        }
+
+    }
+}
